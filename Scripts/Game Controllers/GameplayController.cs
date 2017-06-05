fda3daf1a8d8aa30b94021a97bcc2cb2410ec723
zhixe <amsyar1989@gmail.com>
2017-06-05 20:48:05 +0800

    Update scripts

---
+++ Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour {
+
+    public static GameplayController instance;
+
+    [HideInInspector]
+    public bool gamePlaying;
+
+	// Use this for initialization
+	void Awake () {
+        MakeSingleton();
+	}
+
+    void OnDisabble ()
+    {
+        instance = null;
+    }
+	
+	// Update is called once per frame
+	void MakeSingleton () {
+		if (instance == null)
+        {
+            instance = this;
+        }
+	}
+}
