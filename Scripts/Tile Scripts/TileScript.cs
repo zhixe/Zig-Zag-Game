ad95bd5d88f027435b4774e9afb2677c056f1240
zhixe <amsyar1989@gmail.com>
2017-06-05 22:35:46 +0800

    Update scripts

--- Scripts/Tile Scripts/TileScript.cs
+++ Scripts/Tile Scripts/TileScript.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
+    private Rigidbody myBody;
+    private AudioSource audioSource;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        myBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator TriggerFallingDown()
+    {
+        yield return new WaitForSeconds(0.3f);
+        myBody.isKinematic = false;
+        audioSource.Play();
+        StartCoroutine(TurnOffGameObject());
+    }
+
+    IEnumerator TurnOffGameObject()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.tag == "Ball")
+        {
+            StartCoroutine(TriggerFallingDown());
+        }
+    }
 }
