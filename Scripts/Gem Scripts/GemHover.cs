67446d2fa50bb6fdf0c6fd527a8ab75d3755f3b2
zhixe <amsyar1989@gmail.com>
2017-06-05 22:57:34 +0800

    Update scripts

---
+++ Scripts/Gem Scripts/GemHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemHover : MonoBehaviour {
+
+    private float startY;
+    private float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+        startY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+        position = new Vector3(position.x, startY + Mathf.Sin(Time.time * speed) * 0.1f, position.z);
+        transform.position = position;
+    }
+}
---
+++ Scripts/Gem Scripts/GemRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotate : MonoBehaviour {
+
+    private float speed = 1f;
+    private float angle;
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+
+        angle = (angle + speed) % 360f;
+        transform.localRotation = Quaternion.Euler(new Vector3(45f, angle, 0f));
+	}
+}
